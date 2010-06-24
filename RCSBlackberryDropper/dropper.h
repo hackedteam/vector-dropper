25f2a78b1fef35c5bddb64886424b0e1ae1ad1d8
alor <alor@30e694c7-159d-4bd8-ba75-215c342f8d20>
2010-06-24 11:36:07 +0000

    removed unwanted projects
    git-svn-id: http://rcs-dev/svn/rcs/RCSBuilder@2563 30e694c7-159d-4bd8-ba75-215c342f8d20

--- RCSBlackberryDropper/dropper.h
+++
@@ -1,8 +0,0 @@
-
-#define ERROR_OUTPUT			-1
-#define ERROR_EMBEDDING			-2
-#define ERROR_POLYMER			-3
-#define ERROR_MANIFEST			-4
-#define ERROR_NO_DROPPER		-100
-#define ERROR_INVALID_DROPPER	-101
-

